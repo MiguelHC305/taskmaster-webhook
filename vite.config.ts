3e1cd06c68ee437b9ef9a8debada68ce01b197b2
MiguelHC305 <mhc638417@gmail.com>
2025-08-09 23:35:27 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,15 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-async function getPlugins() {
-  const plugins = [
-    react(),
-    runtimeErrorOverlay()
-  ];
-
-  if (
-    process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-  ) {
-    const { cartographer } = await import("@replit/vite-plugin-cartographer");
-    plugins.push(cartographer());
+function getPlugins() {
+  const plugins = [react(), runtimeErrorOverlay()];
+  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
+    return import("@replit/vite-plugin-cartographer").then((m) => {
+      plugins.push(m.cartographer());
+      return plugins;
+    });
   }
-
-  return plugins;
+  return Promise.resolve(plugins);
 }
 
 export default defineConfig(async () => {
@@ -27,7 +19,7 @@ export default defineConfig(async () => {
     plugins: await getPlugins(),
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "client", "src"),
+        "@": path.resolve(__dirname, "client/src"),
         "@shared": path.resolve(__dirname, "shared"),
         "@assets": path.resolve(__dirname, "attached_assets"),
       },
@@ -45,3 +37,4 @@ export default defineConfig(async () => {
     },
   };
 });
+
